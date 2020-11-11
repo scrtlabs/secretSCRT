eb24a54869afdc28f2f174345534a8e16025eb9d
PumpkinSeed <qwer.kocka@gmail.com>
2020-11-11 08:00:59 +0000

    Format package imports

--- src/viewing_key.rs
+++ src/viewing_key.rs
@@ -4,8 +4,8 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Env;
-
 use secret_toolkit::crypto::{sha_256, Prng};
+
 use crate::utils::{create_hashed_password, ct_slice_compare};
 
 pub const VIEWING_KEY_SIZE: usize = 32;
